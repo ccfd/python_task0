fc80eadff197b178fe0ed39826a3b692fafe8ffe
marekplochow <fx555jn86>
2016-06-08 18:09:05 +0200

    zadania domowe 1-5

---
+++ solution/test.py
@@ -0,0 +1,17 @@
+from solution import *
+
+
+print Newton(200,2)
+print""
+print Pascal(6)
+print""
+LotOfHash(6)
+print""
+PowerModulo(2,200,100)
+print""
+Intersect( (0,0,5), (6,0,5) )
+# [ (3, 4), (3,-4) ]
+Intersect( (0,0,5), (10,0,5) )
+# [ (5, 0) ]
+Intersect( (0,0,5), (15,0,5) )
+# [ ]
